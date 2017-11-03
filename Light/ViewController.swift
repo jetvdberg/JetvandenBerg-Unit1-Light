4d0078f88c02d366c3389aed2b005e6ecc24ee44
jetvdberg <jet.vd.berg@gmail.com>
2017-11-03 11:30:25 +0100

    Added: Background changes color when button is touched, done by connections

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+var lightOn = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func buttonPressed(_ sender: AnyObject) {
+        lightOn = !lightOn
+        updateUI()
+    }
+    
+    func updateUI() {
+        view.backgroundColor = lightOn ? .white : .black
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
